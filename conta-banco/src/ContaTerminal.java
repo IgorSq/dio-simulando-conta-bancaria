e39b5f5f34156f2300b49a761b6b8e27230c9322
IgorSq <igor.quintiliano.silva@gmail.com>
2025-02-19 15:32:12 -0300

    Conta Criada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,43 @@
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //TODO: Exibir as menasagens
+        //obter os valores do usuário
+        //exibir a conta criada
+        Locale.setDefault(Locale.BRAZIL);
+
+        Scanner sc = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println( "Por favor, digite o seu Nome: ");
+        nomeCliente = sc.nextLine();
+        System.out.println();
+
+        System.out.println( "Por favor, digite o número da Agência: ");
+        agencia = sc.nextLine();
+        System.out.println();
+
+        System.out.println( "Agora, digite o número da sua Conta: ");
+        numeroConta = sc.nextInt();
+        System.out.println();
+
+        System.out.println( "Agora, digite o seu saldo: ");
+        saldo = sc.nextDouble();
+        System.out.println();
+
+
+        System.out.printf("Olá %s , obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia , numeroConta, saldo );
+        sc.close();
+
+
+
     }
 }
